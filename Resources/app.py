84120e19ee235ab58484098ced3daee3bc16f733
alanacsaposs <alanacsaposs@gmail.com>
2019-09-03 12:00:54 -0700

    App.py fixed

--- Resources/app.py
+++ Resources/app.py
@@ -40,8 +40,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start (start must be written in YYYY-MM-DD format eg. /api/v1.0/2014-08-09)<br/>"
+        f"/api/v1.0/start/end (start and end must be written in YYYY-MM-DD format eg. /api/v1.0/2014-08-09/2015-08-09)"
     )
 
 
@@ -90,7 +90,7 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 #query for the dates and temperature observations from a year from the last data point.
-#note: last data point is 
+#note: last data point is 08-23-17
 #Return a JSON list of Temperature Observations (tobs) for the previous year.
 
 #Define a year from last data point
@@ -115,6 +115,39 @@ def tobs():
   
 
     return jsonify(all_tobs)
+
+#* `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+#* Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+#* When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+#* When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+
+@app.route("/api/v1.0/<start>")
+def start(start=None):
+    # Create our session (link) from Python to the DB
+    # start must be in YYYY-MM-DD
+    session = Session(engine)
+
+    from_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    from_start_list=list(from_start)
+    return jsonify(from_start_list)
+
+    session.close()
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
+    # Create our session (link) from Python to the DB
+    # start and end must be in YYYY-MM-DD
+    session = Session(engine)
+   
+    
+    start_end_dates = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+    start_end_dates_list=list(start_end_dates)
+    return jsonify(start_end_dates_list)
+
+    session.close()
+
+
+
 
 
 if __name__ == '__main__':
